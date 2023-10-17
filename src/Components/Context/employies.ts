eb38a6c01f44e10ac4497e7e57a2d802d6087550
sbrandon0432 <sbrandon0432@yahoo.com>
2023-10-17 14:01:27 -0400

    added user img default free use

---
+++ src/Components/Context/employies.ts
@@ -0,0 +1,41 @@
+import DefaultUser from "../../Assets/Images/DefaultUser.jpg";
+import { EmployeeCardProps } from "./types";
+
+export const employees: EmployeeCardProps[] = [
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+    {
+        image: DefaultUser,
+        hobby: "D&D",
+        position: "Owner",
+        about: "My favorite thing about working at Level up is meeting all the wonderful people in the communities",
+    },
+];
