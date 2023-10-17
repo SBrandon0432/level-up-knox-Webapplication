a4432e63a407ca61367ab579309538fddc865ac9
sbrandon0432 <sbrandon0432@yahoo.com>
2023-10-17 13:39:12 -0400

    navigating to meet team component

---
+++ src/Components/MeetTeam/MeetTeam.tsx
@@ -0,0 +1,9 @@
+import "./MeetTeamS.scss";
+
+export const MeetTeam = () => {
+    return (
+        <div className="MeetTeam">
+            <h1>hello</h1>
+        </div>
+    );
+};
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../Assets/Svgs/MainLogoKnox.svg";
+import { Paths } from "../../Types/types";
 import { Contact } from "../Contact/Contact";
 import "./NavBarS.scss";
 
@@ -10,14 +11,14 @@ export const NavBar = () => {
         <Navbar className="NavBar" fixed="top">
             <div className="background"></div>
             <Container>
-                <Navbar.Brand href="/home">
+                <Navbar.Brand href={Paths.HOME}>
                     <div className="logo">
                         <img src={logo} className="image" alt="logo" />
                     </div>
                 </Navbar.Brand>
                 <div className="tabs">
                     <Contact />
-                    <Nav.Link href="/" className="tab">
+                    <Nav.Link href={Paths.MEETTEAM} className="tab">
                         Meet Our Team
                     </Nav.Link>
                 </div>
